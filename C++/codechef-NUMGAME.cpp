5721adcf4d0f57cab09e7839548f6ea966f1aff6
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2020-10-14 15:48:52 +0200

    New problem

---
+++ C++/codechef-NUMGAME.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+
+int main(int argc, char const *argv[]) {
+  std::ios::sync_with_stdio(false);
+  unsigned int t;
+  unsigned int n;
+
+  cin >> t;
+
+  for (unsigned int i = 0; i < t; ++i) {
+    cin >> n;
+    if (n % 2 == 0)
+      cout << "ALICE\n";
+    else
+      cout << "BOB\n";
+  }
+}
