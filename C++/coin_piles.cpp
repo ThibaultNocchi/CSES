bae1dbca099ae8b3be36d27726aedce0173ece51
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2020-10-14 14:06:48 +0200

    Added new problem

---
+++ C++/coin_piles.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::string;
+
+int main(int argc, char const *argv[]) {
+  std::ios::sync_with_stdio(false);
+  unsigned int t;
+  unsigned int a, b;
+
+  cin >> t;
+
+  for (unsigned int i = 0; i < t; ++i) {
+    cin >> a >> b;
+    if (a == 0 && b == 0)
+      cout << "YES\n";
+    else if ((a == 0 && b != 0) || (a != 0 && b == 0))
+      cout << "NO\n";
+    else if ((a < b && a * 2 < b) || (b < a && b * 2 < a))
+      cout << "NO\n";
+    else if ((a + b) % 3 == 0)
+      cout << "YES\n";
+    else
+      cout << "NO\n";
+  }
+}
