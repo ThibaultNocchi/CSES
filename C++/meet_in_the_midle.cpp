1279ce009e1a29733c5ab0506202cc7dbb4f299d
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2020-10-16 19:36:37 +0200

    further optims and increasing coverage of tests

--- C++/meet_in_the_midle.cpp
+++ C++/meet_in_the_midle.cpp
@@ -3,18 +3,22 @@
 #include <cmath>
 #include <cstdint>
 #include <cstdio>
+#include <unordered_map>
 #include <vector>
 
+using std::random_shuffle;
 using std::sort;
+using std::unordered_map;
 using std::vector;
 
-void combinations_sums(const vector<uint32_t> &vec,
-                       vector<uint32_t> &vec_results, uint32_t &x) {
+void combinations_sums(const vector<uint64_t> &vec,
+                       unordered_map<uint64_t, uint64_t> &map_results,
+                       uint64_t &x) {
   uint8_t vec_size = vec.size();
-  uint32_t nbr_combinations = pow(2, vec_size);
-  uint32_t sum;
+  uint64_t nbr_combinations = pow(2, vec_size);
+  uint64_t sum;
 
-  for (uint32_t i = 0; i < nbr_combinations; ++i) {
+  for (uint64_t i = 0; i < nbr_combinations; ++i) {
     sum = 0;
     for (uint8_t j = 0; j < vec_size; ++j) {
       if (i & (1 << j)) {
@@ -24,7 +28,7 @@ void combinations_sums(const vector<uint32_t> &vec,
       }
     }
     if (sum <= x)
-      vec_results.push_back(sum);
+      ++map_results[sum];
   }
 }
 
@@ -33,55 +37,33 @@ int main(int argc, char const *argv[]) {
   std::ios_base::sync_with_stdio(false);
 
   uint8_t n;
-  uint32_t x;
-  uint32_t ti;
+  uint64_t x;
+  uint64_t ti;
 
-  vector<uint32_t> t;
+  vector<uint64_t> t;
 
-  scanf("%hhd %d", &n, &x);
+  scanf("%hhd %ld", &n, &x);
 
   for (uint8_t i = 0; i < n; ++i) {
-    scanf("%d", &ti);
+    scanf("%ld", &ti);
     t.push_back(ti);
   }
 
-  uint8_t middle = n / 2;
-  vector<uint32_t> t1(t.begin(), t.begin() + middle);
-  vector<uint32_t> t2(t.begin() + middle, t.end());
+  random_shuffle(t.begin(), t.end());
 
-  vector<uint32_t> t1_sums, t2_sums;
+  uint8_t middle = n / 2;
+  vector<uint64_t> t1(t.begin(), t.begin() + middle);
+  vector<uint64_t> t2(t.begin() + middle, t.end());
+
+  unordered_map<uint64_t, uint64_t> t1_sums, t2_sums;
 
   combinations_sums(t1, t1_sums, x);
   combinations_sums(t2, t2_sums, x);
 
-  vector<uint32_t> *small, *big;
-
-  if (t1_sums.size() < t2_sums.size()) {
-    small = &t1_sums;
-    big = &t2_sums;
-  } else {
-    big = &t1_sums;
-    small = &t2_sums;
-  }
-
-  sort(small->begin(), small->end());
-
-  // for (const auto &el : *small) {
-  //   printf("%d ", el);
-  // }
-  // printf("\n");
-
   uint64_t equal_combinations = 0;
-  uint32_t both_sum;
 
-  for (const auto &sum_t1 : *big) {
-    for (const auto &sum_t2 : *small) {
-      both_sum = sum_t1 + sum_t2;
-      if (both_sum == x)
-        ++equal_combinations;
-      else if (both_sum > x)
-        break;
-    }
+  for (const auto &el : t1_sums) {
+    equal_combinations += el.second * t2_sums[x - el.first];
   }
 
   printf("%ld\n", equal_combinations);
