08c76e8189f8cf1dc617397bd779dd1dc768e92a
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2020-10-19 11:55:38 +0200

    working solution for meet in the middle

--- C++/meet_in_the_midle.cpp
+++ C++/meet_in_the_midle.cpp
@@ -6,14 +6,15 @@
 #include <unordered_map>
 #include <vector>
 
+using std::lower_bound;
 using std::random_shuffle;
 using std::sort;
 using std::unordered_map;
+using std::upper_bound;
 using std::vector;
 
 void combinations_sums(const vector<uint64_t> &vec,
-                       unordered_map<uint64_t, uint64_t> &map_results,
-                       uint64_t &x) {
+                       vector<uint64_t> &vec_results, uint64_t &x) {
   uint8_t vec_size = vec.size();
   uint64_t nbr_combinations = pow(2, vec_size);
   uint64_t sum;
@@ -28,7 +29,7 @@ void combinations_sums(const vector<uint64_t> &vec,
       }
     }
     if (sum <= x)
-      ++map_results[sum];
+      vec_results.push_back(sum);
   }
 }
 
@@ -53,15 +54,21 @@ int main(int argc, char const *argv[]) {
   vector<uint64_t> t1(t.begin(), t.begin() + middle);
   vector<uint64_t> t2(t.begin() + middle, t.end());
 
-  unordered_map<uint64_t, uint64_t> t1_sums, t2_sums;
+  vector<uint64_t> t1_sums, t2_sums;
 
   combinations_sums(t1, t1_sums, x);
   combinations_sums(t2, t2_sums, x);
 
+  sort(t2_sums.begin(), t2_sums.end());
+
   uint64_t equal_combinations = 0;
 
   for (const auto &el : t1_sums) {
-    equal_combinations += el.second * t2_sums[x - el.first];
+    uint64_t target = x - el;
+    auto it1 = lower_bound(t2_sums.begin(), t2_sums.end(), target);
+    auto it2 = upper_bound(t2_sums.begin(), t2_sums.end(), target);
+    if (it1 != t2_sums.end() && *it1 == target)
+      equal_combinations += it2 - it1;
   }
 
   printf("%ld\n", equal_combinations);
